d74c7130cfd474e8deb54c97a93a9c7ffa693c1c
Vladimir Poplavskij <float45@gmail.com>
2025-08-02 00:40:31 +0300

    MTM-17: Update tests for triggers

--- src/triggers/triggers.service.spec.ts
+++ src/triggers/triggers.service.spec.ts
@@ -9,20 +9,16 @@ import { UpdateTriggerDto } from './dto/update-trigger.dto';
 import { NotFoundException } from '@nestjs/common';
 
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-// TODO after fix tests remove
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/unbound-method */
 
-// Mock data
 const mockTrigger: HydratedDocument<Trigger> = {
-  _id: new Types.ObjectId('60c72b2f9b1d8e001c8e4d3a'), // Example ObjectId
+  _id: new Types.ObjectId('60c72b2f9b1d8e001c8e4d3a'),
   userId: 'user123',
   type: 'Headache',
   note: 'Started after stress',
   createdAt: new Date('2023-01-01T10:00:00Z'),
   datetimeAt: new Date('2023-01-01T12:00:00Z'),
-} as any; // Cast as any to satisfy Document interface if not fully implemented in mock
+} as any;
 
 const mockTriggers: HydratedDocument<Trigger>[] = [
   mockTrigger,
@@ -36,55 +32,50 @@ const mockTriggers: HydratedDocument<Trigger>[] = [
   },
 ] as any;
 
-// Mock the Mongoose Model methods
-const mockTriggerModel = {
-  // Mock 'save' for create operations
-  create: jest.fn().mockImplementation((dto: CreateTriggerDto) =>
-    Promise.resolve({
-      _id: new Types.ObjectId(), // Generate a new ID for the created item
-      ...dto,
-      createdAt: new Date(),
-      toObject: () => ({
-        _id: new Types.ObjectId(),
-        ...dto,
-        createdAt: new Date(),
-      }),
-    } as HydratedDocument<Trigger>),
-  ),
-
-  // For find operations
-  find: jest.fn().mockReturnThis(), // Allow chaining .exec()
-  findById: jest.fn().mockReturnThis(), // Allow chaining .exec()
-  exec: jest.fn(), // This will be chained after find/findById
-
-  // For update operations
-  findByIdAndUpdate: jest.fn().mockReturnThis(),
-
-  // For delete operations
-  deleteOne: jest.fn().mockReturnThis(),
-};
-
 describe('TriggersService', () => {
   let service: TriggersService;
-  let model: Model<TriggerDocument>;
+  let mockTriggerModel: jest.Mocked<Model<TriggerDocument>>;
+  let mockDocumentInstance: TriggerDocument;
 
   beforeEach(async () => {
+    mockDocumentInstance = {
+      ...mockTrigger,
+      save: jest.fn().mockResolvedValue(mockTrigger),
+    } as unknown as TriggerDocument;
+
+    mockTriggerModel = jest.fn().mockImplementation(() => {
+      return mockDocumentInstance;
+    }) as unknown as jest.Mocked<Model<TriggerDocument>>;
+
+    mockTriggerModel.find = jest.fn().mockReturnValue({
+      exec: jest.fn().mockResolvedValue(mockTriggers),
+    });
+    mockTriggerModel.findById = jest.fn().mockReturnValue({
+      exec: jest.fn().mockResolvedValue(mockTrigger),
+    });
+    mockTriggerModel.create = jest.fn().mockResolvedValue(mockDocumentInstance);
+    mockTriggerModel.findByIdAndUpdate = jest.fn().mockReturnValue({
+      exec: jest.fn().mockResolvedValue(mockDocumentInstance),
+    });
+    mockTriggerModel.deleteOne = jest.fn().mockReturnValue({
+      exec: jest.fn().mockResolvedValue({ deletedCount: 1 }),
+    });
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         TriggersService,
         {
-          provide: getModelToken('Trigger'), // Use the name of your Mongoose model
+          provide: getModelToken('Trigger'),
           useValue: mockTriggerModel,
         },
       ],
     }).compile();
 
     service = module.get<TriggersService>(TriggersService);
-    model = module.get<Model<TriggerDocument>>(getModelToken('Trigger'));
   });
 
   afterEach(() => {
-    jest.clearAllMocks(); // Clear mocks after each test
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
@@ -92,7 +83,7 @@ describe('TriggersService', () => {
   });
 
   describe('create', () => {
-    it.skip('should create and return a trigger', async () => {
+    it('should create and return a trigger', async () => {
       const createDto: CreateTriggerDto = {
         userId: 'testUser',
         type: 'TestType',
@@ -100,41 +91,23 @@ describe('TriggersService', () => {
         datetimeAt: new Date(),
       };
 
-      // Mock the save method for the instance created by `new this.triggerModel(createTriggerDto)`
-      const mockSave = jest.fn().mockResolvedValue(mockTrigger);
-      jest.spyOn(model, 'create').mockImplementationOnce(
-        () =>
-          ({
-            save: mockSave,
-          }) as any,
-      ); // Mock the instance's save method
-
-      // Temporarily mock the constructor of the model to return an object with a save method
-      const modelConstructorSpy = jest
-        .spyOn(model.constructor as any, 'name', 'get')
-        .mockReturnValue('Trigger'); // Mock the name property
-      //TODO fix test type
-      /*jest.spyOn(model, 'constructor').mockImplementation(function (data) {
-        this.save = mockSave;
-        Object.assign(this, data);
-        return this;
-      } as any);*/
-
       const result = await service.create(createDto);
 
-      expect(mockSave).toHaveBeenCalled();
-      expect(result).toEqual({
-        id: mockTrigger._id.toString(), // Expect id as string
-        ...mockTrigger,
-        _id: undefined, // _id should not be in the final ITrigger object
-      });
+      expect(mockTriggerModel).toHaveBeenCalledWith(createDto);
 
-      // Restore original constructor if needed, though afterEach clears mocks
-      modelConstructorSpy.mockRestore();
+      expect(mockDocumentInstance.save).toHaveBeenCalled();
+
+      expect(result).toEqual({
+        id: mockTrigger._id.toString(),
+        userId: mockTrigger.userId,
+        type: mockTrigger.type,
+        note: mockTrigger.note,
+        createdAt: mockTrigger.createdAt,
+        datetimeAt: mockTrigger.datetimeAt,
+      });
     });
 
-    // Simpler mock for create, assuming `create` directly on the model
-    it.skip('should create a trigger (simpler mock for Model.create)', async () => {
+    it('should create a trigger (simpler mock for Model.create)', async () => {
       const createDto: CreateTriggerDto = {
         userId: 'testUser',
         type: 'TestType',
@@ -142,14 +115,11 @@ describe('TriggersService', () => {
         datetimeAt: new Date(),
       };
 
-      // Mock Model.create to return a mocked document
-      // TODO fix test
-      //jest.spyOn(model, 'create').mockResolvedValueOnce(mockTrigger);
-      const createSpy = jest.spyOn(model, 'create');
-
       const result = await service.create(createDto);
 
-      expect(createSpy).toHaveBeenCalledWith(createDto);
+      expect(mockTriggerModel).toHaveBeenCalledWith(createDto);
+      expect(mockDocumentInstance.save).toHaveBeenCalled();
+
       expect(result).toEqual({
         id: mockTrigger._id.toString(),
         userId: mockTrigger.userId,
@@ -163,12 +133,9 @@ describe('TriggersService', () => {
 
   describe('findAll', () => {
     it('should return an array of triggers', async () => {
-      mockTriggerModel.exec.mockResolvedValueOnce(mockTriggers); // Mock exec for find()
-      const findSpy = jest.spyOn(model, 'find');
-
       const result = await service.findAll();
 
-      expect(findSpy).toHaveBeenCalled();
+      expect(mockTriggerModel.find).toHaveBeenCalled();
       expect(result).toEqual(
         mockTriggers.map((t) => ({
           id: t._id.toString(),
@@ -184,12 +151,11 @@ describe('TriggersService', () => {
 
   describe('findOne', () => {
     it('should return a single trigger', async () => {
-      mockTriggerModel.exec.mockResolvedValueOnce(mockTrigger); // Mock exec for findById()
-      const findByIdSpy = jest.spyOn(model, 'findById');
-
       const result = await service.findOne(mockTrigger._id.toHexString());
 
-      expect(findByIdSpy).toHaveBeenCalledWith(mockTrigger._id.toHexString());
+      expect(mockTriggerModel.findById).toHaveBeenCalledWith(
+        mockTrigger._id.toHexString(),
+      );
       expect(result).toEqual({
         id: mockTrigger._id.toString(),
         userId: mockTrigger.userId,
@@ -201,8 +167,9 @@ describe('TriggersService', () => {
     });
 
     it('should throw NotFoundException if trigger not found', async () => {
-      mockTriggerModel.exec.mockResolvedValueOnce(null); // Mock exec to return null
-
+      mockTriggerModel.findById = jest.fn().mockReturnValue({
+        exec: () => null,
+      });
       await expect(service.findOne('nonExistentId')).rejects.toThrow(
         NotFoundException,
       );
@@ -217,15 +184,16 @@ describe('TriggersService', () => {
         type: mockTrigger.type,
       };
       const updatedMockTrigger = { ...mockTrigger, note: 'Updated note' };
-      const findByIdAndUpdateSpy = jest.spyOn(model, 'findByIdAndUpdate');
-      mockTriggerModel.exec.mockResolvedValueOnce(updatedMockTrigger); // Mock exec for findByIdAndUpdate()
+      mockTriggerModel.findByIdAndUpdate = jest.fn().mockReturnValue({
+        exec: () => updatedMockTrigger,
+      });
 
       const result = await service.update(
         mockTrigger._id.toHexString(),
         updateDto,
       );
 
-      expect(findByIdAndUpdateSpy).toHaveBeenCalledWith(
+      expect(mockTriggerModel.findByIdAndUpdate).toHaveBeenCalledWith(
         mockTrigger._id.toHexString(),
         updateDto,
         { new: true },
@@ -241,7 +209,9 @@ describe('TriggersService', () => {
     });
 
     it('should throw NotFoundException if trigger not found during update', async () => {
-      mockTriggerModel.exec.mockResolvedValueOnce(null); // Mock exec to return null
+      mockTriggerModel.findByIdAndUpdate = jest.fn().mockReturnValue({
+        exec: () => null,
+      });
 
       await expect(
         service.update('nonExistentId', {
@@ -253,34 +223,30 @@ describe('TriggersService', () => {
     });
   });
 
-  // TODO fix test
-  describe.skip('remove', () => {
+  describe('remove', () => {
     it('should remove a trigger', async () => {
-      jest
-        .spyOn(mockTriggerModel.deleteOne().exec(), 'mockResolvedValueOnce') // Spy on the exec method
-        .mockResolvedValueOnce({ deletedCount: 1 }); // Mock deleteOne
-      const removeSpy = jest.spyOn(service, 'remove');
+      mockTriggerModel.deleteOne = jest.fn().mockReturnValue({
+        exec: () => Promise.resolve({ deletedCount: 1 }),
+      });
 
       await service.remove(mockTrigger._id.toHexString());
 
-      expect(removeSpy).toHaveBeenCalledWith({
+      expect(mockTriggerModel.deleteOne).toHaveBeenCalledWith({
         _id: mockTrigger._id.toHexString(),
       });
     });
 
     it('should throw NotFoundException if trigger not found during remove', async () => {
-      const removeSpy = jest
-        .spyOn(service, 'remove')
-        .mockRejectedValueOnce(new NotFoundException());
-
-      jest
-        .spyOn(mockTriggerModel.deleteOne().exec(), 'mockResolvedValueOnce') // Spy on the exec method
-        .mockResolvedValueOnce({ deletedCount: 0 }); // Mock no deletion
+      mockTriggerModel.deleteOne = jest.fn().mockReturnValue({
+        exec: () => Promise.resolve({ deletedCount: 0 }),
+      });
 
       await expect(service.remove('nonExistentId')).rejects.toThrow(
         NotFoundException,
       );
-      expect(removeSpy).toHaveBeenCalledWith('nonExistentId');
+      expect(mockTriggerModel.deleteOne).toHaveBeenCalledWith({
+        _id: 'nonExistentId',
+      });
     });
   });
 });
